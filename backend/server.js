0b22f2e65018e5e66a83d68528f847f4f4c4ca4d
Elie-Dev250 <iradukundaelie71@gmail.com>
2025-05-10 17:56:23 +0200

    making register page

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ import express from "express";
 import { studentsRouter } from "./router/router.js";
 const app = express();
 const port = process.env.SERVER_PORT;
+import cors from 'cors'; 
+
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.use(express.json());
 // process.config();
 app.use("/API/student", studentsRouter)
+app.use('/API/admin',studentsRouter)
 app.listen(port, () => {
     console.log("Azure server: http://localhost:",port);
 })
